f77e6049bd54e7f6151177228b49f5a006a233b1
Iqza Ardiansyah <124988403+iqzaardiansyah@users.noreply.github.com>
2024-05-13 20:32:30 +0700

    [CHORE] Add Continuous Deployment to GCP and integration with SonarCloud

--- src/main/java/com/bezkoder/springjwt/controllers/AuthController.java
+++ src/main/java/com/bezkoder/springjwt/controllers/AuthController.java
@@ -2,19 +2,19 @@ package com.bezkoder.springjwt.controllers;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import jakarta.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,25 +31,34 @@ import com.bezkoder.springjwt.repository.RoleRepository;
 import com.bezkoder.springjwt.repository.UserRepository;
 import com.bezkoder.springjwt.security.jwt.JwtUtils;
 import com.bezkoder.springjwt.security.services.UserDetailsImpl;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-  @Autowired
+  final
   AuthenticationManager authenticationManager;
 
-  @Autowired
+  final
   UserRepository userRepository;
 
-  @Autowired
+  final
   RoleRepository roleRepository;
 
-  @Autowired
+  final
   PasswordEncoder encoder;
 
-  @Autowired
+  final
   JwtUtils jwtUtils;
+
+  public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder encoder, JwtUtils jwtUtils) {
+    this.authenticationManager = authenticationManager;
+    this.userRepository = userRepository;
+    this.roleRepository = roleRepository;
+    this.encoder = encoder;
+    this.jwtUtils = jwtUtils;
+  }
 
   @PostMapping("/signin")
   public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
@@ -60,10 +69,10 @@ public class AuthController {
     SecurityContextHolder.getContext().setAuthentication(authentication);
     String jwt = jwtUtils.generateJwtToken(authentication);
     
-    UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();    
+    UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
     List<String> roles = userDetails.getAuthorities().stream()
-        .map(item -> item.getAuthority())
-        .collect(Collectors.toList());
+            .map(GrantedAuthority::getAuthority)
+            .toList();
 
     return ResponseEntity.ok(new JwtResponse(jwt, 
                         userDetails.getId(), 
@@ -75,7 +84,7 @@ public class AuthController {
 
   @PostMapping("/signup")
   public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
-    if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+    if (Boolean.TRUE.equals(userRepository.existsByEmail(signUpRequest.getEmail()))) {
       return ResponseEntity
           .badRequest()
           .body(new MessageResponse("Error: Email is already in use!"));
@@ -120,5 +129,31 @@ public class AuthController {
     userRepository.save(user);
 
     return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+  }
+
+  @GetMapping("/getAll")
+  public ResponseEntity<List<User>> getAllUsers() {
+    return ResponseEntity.ok(userRepository.findAll());
+  }
+
+  @GetMapping("/getUserById")
+  public ResponseEntity<?> getUserById(@RequestParam Long id) {
+    Optional<User> optionalUser = userRepository.findById(id);
+    if (optionalUser.isPresent()) {
+      return ResponseEntity.ok(optionalUser.get());
+    } else {
+      return ResponseEntity.badRequest().body(new MessageResponse("Error: User is not found!"));
+    }
+  }
+
+  @GetMapping("/getUserRole")
+  public ResponseEntity<?> getUserRole(@RequestParam Long id) {
+    Optional<User> optionalUser = userRepository.findById(id);
+    if (optionalUser.isPresent()) {
+      Set<Role> roles = optionalUser.get().getRoles();
+      return ResponseEntity.ok(roles);
+    } else {
+      return ResponseEntity.badRequest().body(new MessageResponse("Error: User is not found!"));
+    }
   }
 }
